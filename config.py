0faa97304b22720eb9c780388324b67ccc719948
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-05-15 12:28:44 +0700

    config database mysql

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+     MYSQL_HOST = 'localhost'
+     MYSQL_USER = 'root'
+     MYSQL_PASSWORD = ''
+     MYSQL_DB = 'guru_honorer'
+     SECRET_KEY = 'teacher_honorer'
+     DEBUG = True
