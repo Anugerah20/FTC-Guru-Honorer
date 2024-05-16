911db2d5359ae91bdc757c0e0014ab1f0045f846
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-05-16 17:22:54 +0700

    update: app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-# app.py
 from flask import Flask
 from config import Config
 from extensions import mysql, cors
 from blueprints.auth import auth
 from blueprints.main import main
-
-import pandas as pd
-from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Upload file
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mysql.init_app(app)
 cors.init_app(app)
@@ -18,4 +19,9 @@ app.register_blueprint(auth)
 app.register_blueprint(main)
 
 if __name__ == '__main__':
+     # Buat folder 'uploads' jika belum ada
+     if not os.path.exists(app.config['UPLOAD_FOLDER']):
+          os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
      app.run(debug=True)
