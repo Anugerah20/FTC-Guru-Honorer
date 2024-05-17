ad98c08eba5d1e05fdef23ac92cd938980eaa09b
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-05-17 10:43:51 +0700

    update: name file training and testing

--- blueprints/main.py
+++ blueprints/main.py
@@ -440,7 +440,7 @@ def trainModel():
 
     cur.close()
     # return jsonify({'message': 'Model berhasil dilatih'})
-    return render_template('train-model.html', current_url=request.path ,username=username)
+    return render_template('train-test.html', current_url=request.path ,username=username)
 
 # Route membuat tabel data testing
 @main.route('/data-testing')
