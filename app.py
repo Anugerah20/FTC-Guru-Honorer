21965a5abc48b4629e437bb128628a86e8ab638a
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-05-19 18:25:03 +0700

    update: migrate SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import Config
-from extensions import mysql, cors
+from extensions import mysql, cors, db
 from blueprints.auth import auth
 from blueprints.main import main
+from blueprints.labeling import labeling
 import os
+# Migrate SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
 # Upload file
@@ -14,14 +18,19 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 mysql.init_app(app)
 cors.init_app(app)
+db.init_app(app)
 
+# Flask Migrate SQLAlchemy
+migrate = Migrate(app, db, compare_type=True)
+
+# Register blueprint
 app.register_blueprint(auth)
 app.register_blueprint(main)
+app.register_blueprint(labeling, app=app)
 
 if __name__ == '__main__':
      # Buat folder 'uploads' jika belum ada
      if not os.path.exists(app.config['UPLOAD_FOLDER']):
           os.makedirs(app.config['UPLOAD_FOLDER'])
-
 
      app.run(debug=True)
