c383397e8bdb772251da8bad0d1ad757182e99dd
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-08-21 13:35:35 +0700

    Update comment pengujian.py

--- pengujian.py
+++ pengujian.py
@@ -1,4 +1,4 @@
-# Contoh hasil clustering dari beberapa iterasi
+# hasil clustering dari beberapa iterasi
 clustering_results = {
     "Iterasi ke-1": {
         "gaji guru honorer": ['D369', 'D328', 'D448', 'D461', 'D92', 'D370', 'D507', 'D4', 'D23', 'D144', 'D240', 'D479', 'D155', 'D47', 'D206', 'D62', 'D213', 'D269', 'D7', 'D389', 'D243', 'D277', 'D280', 'D356', 'D44', 'D197', 'D314', 'D515', 'D5', 'D491', 'D505', 'D54', 'D83', 'D360', 'D388', 'D340', 'D45', 'D432', 'D97', 'D124', 'D96', 'D458', 'D199', 'D237', 'D495', 'D145', 'D265', 'D247', 'D230', 'D163', 'D380', 'D533', 'D322', 'D106', 'D534', 'D492', 'D81', 'D357', 'D113', 'D411', 'D229', 'D105', 'D174', 'D394', 'D203', 'D337', 'D485', 'D115', 'D354', 'D361', 'D253', 'D345', 'D245', 'D317', 'D305', 'D179', 'D141', 'D319', 'D232', 'D513', 'D365', 'D476', 'D132', 'D244', 'D418', 'D258', 'D421', 'D41', 'D61', 'D332', 'D391', 'D297', 'D487', 'D77', 'D267', 'D101', 'D499', 'D468', 'D341', 'D248', 'D511', 'D36', 'D399', 'D34', 'D437', 'D154', 'D484', 'D514', 'D40', 'D262', 'D125', 'D67', 'D390', 'D412', 'D263', 'D384', 'D503', 'D504', 'D406', 'D477', 'D375', 'D270', 'D286', 'D510', 'D24', 'D91', 'D462', 'D478', 'D383', 'D11', 'D291', 'D371', 'D287', 'D241', 'D294', 'D221', 'D136', 'D350', 'D13', 'D151', 'D387', 'D278', 'D307', 'D275', 'D395', 'D252', 'D153', 'D266', 'D128', 'D160', 'D209', 'D378', 'D403', 'D166', 'D33', 'D304', 'D398', 'D188', 'D284', 'D333', 'D430', 'D43', 'D393', 'D506', 'D223', 'D447', 'D3', 'D16', 'D112', 'D264', 'D177', 'D523', 'D440', 'D127', 'D427', 'D527', 'D524', 'D339', 'D149', 'D353', 'D309', 'D49', 'D293', 'D300', 'D71', 'D470', 'D182', 'D299', 'D425', 'D486', 'D454', 'D175', 'D363', 'D201', 'D142', 'D6', 'D364', 'D76', 'D381', 'D512', 'D272', 'D65', 'D217', 'D481', 'D200', 'D500', 'D233', 'D531', 'D509', 'D74', 'D482', 'D2', 'D436', 'D283', 'D94', 'D298', 'D156', 'D402', 'D489', 'D450', 'D129', 'D15', 'D102', 'D379', 'D466', 'D352', 'D171', 'D348', 'D114', 'D338', 'D56', 'D25', 'D46', 'D109', 'D475', 'D66', 'D138', 'D346', 'D257', 'D260', 'D32', 'D186', 'D312', 'D344', 'D374', 'D169', 'D429', 'D86', 'D98', 'D26', 'D191', 'D433', 'D385', 'D116', 'D472', 'D135', 'D497', 'D469', 'D494', 'D234', 'D8', 'D235', 'D122', 'D496', 'D212', 'D173', 'D408', 'D426', 'D214', 'D342', 'D516', 'D446', 'D308', 'D147', 'D143', 'D400', 'D358', 'D51', 'D93', 'D313', 'D279', 'D100', 'D63', 'D431', 'D205', 'D501', 'D522', 'D35', 'D195', 'D434', 'D439', 'D69', 'D207', 'D68', 'D250', 'D38', 'D414', 'D196', 'D261', 'D377', 'D420', 'D162', 'D222', 'D306', 'D55', 'D417', 'D123', 'D73', 'D301', 'D444', 'D288', 'D150', 'D52', 'D158', 'D219', 'D79', 'D530', 'D180', 'D405', 'D302', 'D367', 'D404', 'D21', 'D459', 'D422', 'D157', 'D292', 'D310', 'D17', 'D104', 'D271', 'D210', 'D335', 'D273', 'D20', 'D520', 'D331', 'D37', 'D95', 'D355', 'D376', 'D409', 'D443', 'D463', 'D42', 'D525', 'D428', 'D121', 'D528', 'D146', 'D401', 'D490', 'D29', 'D148', 'D508', 'D435', 'D110', 'D315', 'D415', 'D321', 'D410', 'D349', 'D320', 'D318', 'D99', 'D22', 'D362', 'D442', 'D28', 'D282', 'D84', 'D480', 'D64', 'D296', 'D198', 'D457', 'D126', 'D521', 'D53', 'D120', 'D465', 'D259', 'D251', 'D386', 'D268', 'D281', 'D274', 'D48', 'D416', 'D227', 'D12', 'D152', 'D50', 'D118', 'D103', 'D226', 'D276', 'D351', 'D438', 'D161', 'D225', 'D39', 'D60', 'D80', 'D208', 'D218', 'D236', 'D57', 'D246', 'D373', 'D449', 'D176', 'D451', 'D316', 'D488', 'D10', 'D474', 'D111', 'D336', 'D372', 'D397', 'D187', 'D517', 'D90', 'D519', 'D159', 'D190', 'D189', 'D359', 'D326', 'D368', 'D303', 'D75', 'D194', 'D529', 'D231', 'D238', 'D289', 'D18', 'D256', 'D184', 'D407', 'D323', 'D518', 'D165', 'D185', 'D424', 'D181', 'D441', 'D483', 'D215', 'D295', 'D460', 'D324', 'D493', 'D366', 'D211', 'D1', 'D502', 'D31', 'D452', 'D498', 'D193', 'D325', 'D330', 'D343', 'D78', 'D526', 'D72', 'D382', 'D134', 'D202', 'D89', 'D170', 'D133', 'D453', 'D85', 'D168', 'D9', 'D88', 'D329', 'D396', 'D27', 'D334', 'D392', 'D455', 'D532', 'D164', 'D445', 'D285', 'D30', 'D423', 'D471', 'D419', 'D327', 'D224', 'D70', 'D473', 'D311', 'D82', 'D183', 'D242', 'D347', 'D167', 'D137', 'D467', 'D228', 'D464', 'D413', 'D131', 'D178', 'D220', 'D140', 'D108', 'D130', 'D290', 'D192', 'D19', 'D59', 'D58', 'D117', 'D119', 'D87', 'D172']
@@ -14,7 +14,7 @@ clustering_results = {
     }
 }
 
-# Contoh label dokumen yang diketahui
+# label dokumen
 label_dict = {
     "D369": "gaji guru honorer",
     "D328": "gaji guru honorer",
@@ -37,7 +37,7 @@ label_dict = {
     "D7": "gaji guru honorer",
     "D389": "gaji guru honorer",
     "D243": "gaji guru honorer",
-    "D277": "gaji guru honorer", # Mentok di D277 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D277": "gaji guru honorer", # Mentok di D277 term set gaji guru honorer (jumlah 22)
     "D280": "gaji guru honorer",
     "D356": "gaji guru honorer",
     "D44": "gaji guru honorer",
@@ -59,7 +59,7 @@ label_dict = {
     "D96": "gaji guru honorer",
     "D458": "gaji guru honorer",
     "D199": "gaji guru honorer",
-    "D237": "gaji guru honorer", # Mentok di D237 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D237": "gaji guru honorer", # Mentok di D237 term set gaji guru honorer (jumlah 22)
     "D495": "gaji guru honorer",
     "D145": "gaji guru honorer",
     "D265": "gaji guru honorer",
@@ -80,7 +80,7 @@ label_dict = {
     "D105": "gaji guru honorer",
     "D174": "gaji guru honorer",
     "D394": "gaji guru honorer",
-    "D203": "gaji guru honorer", # Mentok di D203 iterasi 1 term set gaji guru honorer (jumlah 21)
+    "D203": "gaji guru honorer", # Mentok di D203 term set gaji guru honorer (jumlah 21)
     "D337": "gaji guru honorer",
     "D485": "gaji guru honorer",
     "D115": "gaji guru honorer",
@@ -101,7 +101,7 @@ label_dict = {
     "D132": "gaji guru honorer",
     "D244": "gaji guru honorer",
     "D418": "gaji guru honorer",
-    "D258": "gaji guru honorer", # Mentok di D258 iterasi 1 term set gaji guru honorer (jumlah 21)
+    "D258": "gaji guru honorer", # Mentok di D258 term set gaji guru honorer (jumlah 21)
     "D421": "gaji guru honorer",
     "D41": "gaji guru honorer",
     "D61": "gaji guru honorer",
@@ -123,7 +123,7 @@ label_dict = {
     "D437": "gaji guru honorer",
     "D154": "gaji guru honorer",
     "D484": "gaji guru honorer",
-    "D514": "gaji guru honorer", # Mentok di D514 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D514": "gaji guru honorer", # Mentok di D514 term set gaji guru honorer (jumlah 22)
     "D40": "gaji guru honorer",
     "D262": "gaji guru honorer",
     "D125": "gaji guru honorer",
@@ -145,7 +145,7 @@ label_dict = {
     "D462": "gaji guru honorer",
     "D478": "gaji guru honorer",
     "D383": "gaji guru honorer",
-    "D11": "gaji guru honorer", # Mentok di D11 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D11": "gaji guru honorer", # Mentok di D11 term set gaji guru honorer (jumlah 22)
     "D291": "gaji guru honorer",
     "D371": "gaji guru honorer",
     "D287": "gaji guru honorer",
@@ -166,7 +166,7 @@ label_dict = {
     "D266": "gaji guru honorer",
     "D128": "gaji guru honorer",
     "D160": "gaji guru honorer",
-    "D209": "gaji guru honorer", # Mentok di D209 iterasi 1 term set gaji guru honorer (jumlah 21)
+    "D209": "gaji guru honorer", # Mentok di D209 term set gaji guru honorer (jumlah 21)
     "D378": "gaji guru honorer",
     "D403": "gaji guru honorer",
     "D166": "gaji guru honorer",
@@ -188,7 +188,7 @@ label_dict = {
     "D264": "gaji guru honorer",
     "D177": "gaji guru honorer",
     "D523": "gaji guru honorer",
-    "D440": "gaji guru honorer", # Mentok di D440 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D440": "gaji guru honorer", # Mentok di D440 term set gaji guru honorer (jumlah 22)
     "D127": "gaji guru honorer",
     "D427": "gaji guru honorer",
     "D527": "gaji guru honorer",
@@ -209,7 +209,7 @@ label_dict = {
     "D454": "gaji guru honorer",
     "D175": "gaji guru honorer",
     "D363": "gaji guru honorer",
-    "D201": "gaji guru honorer", # Mentok di D201 iterasi 1 term set gaji guru honorer (jumlah 21)
+    "D201": "gaji guru honorer", # Mentok di D201 term set gaji guru honorer (jumlah 21)
     "D142": "gaji guru honorer",
     "D6": "gaji guru honorer",
     "D364": "gaji guru honorer",
@@ -231,7 +231,7 @@ label_dict = {
     "D436": "gaji guru honorer",
     "D283": "gaji guru honorer",
     "D94": "gaji guru honorer",
-    "D298": "gaji guru honorer", # Mentok di D298 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D298": "gaji guru honorer", # Mentok di D298 term set gaji guru honorer (jumlah 22)
     "D156": "gaji guru honorer",
     "D402": "gaji guru honorer",
     "D489": "gaji guru honorer",
@@ -253,7 +253,7 @@ label_dict = {
     "D475": "gaji guru honorer",
     "D66": "gaji guru honorer",
     "D138": "gaji guru honorer",
-    "D346": "gaji guru honorer", # Mentok di D346 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D346": "gaji guru honorer", # Mentok di D346 term set gaji guru honorer (jumlah 22)
     "D257": "gaji guru honorer",
     "D260": "gaji guru honorer",
     "D32": "gaji guru honorer",
@@ -275,7 +275,7 @@ label_dict = {
     "D497": "gaji guru honorer",
     "D469": "gaji guru honorer",
     "D494": "gaji guru honorer",
-    "D234": "gaji guru honorer", # Mentok di D234 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D234": "gaji guru honorer", # Mentok di D234 term set gaji guru honorer (jumlah 22)
     "D8": "gaji guru honorer",
     "D235": "gaji guru honorer",
     "D122": "gaji guru honorer",
@@ -297,7 +297,7 @@ label_dict = {
     "D93": "gaji guru honorer",
     "D313": "gaji guru honorer",
     "D279": "gaji guru honorer",
-    "D100": "gaji guru honorer", # Mentok di D100 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D100": "gaji guru honorer", # Mentok di D100 term set gaji guru honorer (jumlah 22)
     "D63": "gaji guru honorer",
     "D431": "gaji guru honorer",
     "D205": "gaji guru honorer",
@@ -319,7 +319,7 @@ label_dict = {
     "D420": "gaji guru honorer",
     "D162": "gaji guru honorer",
     "D222": "gaji guru honorer",
-    "D306": "gaji guru honorer", # Mentok di D306 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D306": "gaji guru honorer", # Mentok di D306 term set gaji guru honorer (jumlah 22)
     "D55": "gaji guru honorer",
     "D417": "gaji guru honorer",
     "D123": "gaji guru honorer",
@@ -341,7 +341,7 @@ label_dict = {
     "D21": "gaji guru honorer",
     "D459": "gaji guru honorer",
     "D422": "gaji guru honorer",
-    "D157": "gaji guru honorer", # Mentok di D157 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D157": "gaji guru honorer", # Mentok di D157 term set gaji guru honorer (jumlah 22)
     "D292": "gaji guru honorer",
     "D310": "gaji guru honorer",
     "D17": "gaji guru honorer",
@@ -363,7 +363,7 @@ label_dict = {
     "D42": "gaji guru honorer",
     "D525": "gaji guru honorer",
     "D428": "gaji guru honorer",
-    "D121": "gaji guru honorer", # Mentok di D121 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D121": "gaji guru honorer", # Mentok di D121 term set gaji guru honorer (jumlah 22)
     "D528": "gaji guru honorer",
     "D146": "gaji guru honorer",
     "D401": "gaji guru honorer",
@@ -385,7 +385,7 @@ label_dict = {
     "D362": "gaji guru honorer",
     "D442": "gaji guru honorer",
     "D28": "gaji guru honorer",
-    "D282": "gaji guru honorer", # Mentok di D282 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D282": "gaji guru honorer", # Mentok di D282 term set gaji guru honorer (jumlah 22)
     "D84": "gaji guru honorer",
     "D480": "gaji guru honorer",
     "D64": "gaji guru honorer",
@@ -407,7 +407,7 @@ label_dict = {
     "D416": "gaji guru honorer",
     "D227": "gaji guru honorer",
     "D12": "gaji guru honorer",
-    "D152": "gaji guru honorer", # Mentok di D152 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D152": "gaji guru honorer", # Mentok di D152 term set gaji guru honorer (jumlah 22)
     "D50": "gaji guru honorer",
     "D118": "gaji guru honorer",
     "D103": "gaji guru honorer",
@@ -429,7 +429,7 @@ label_dict = {
     "D449": "gaji guru honorer",
     "D176": "gaji guru honorer",
     "D451": "gaji guru honorer",
-    "D316": "gaji guru honorer", # Mentok di D316 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D316": "gaji guru honorer", # Mentok di D316 term set gaji guru honorer (jumlah 22)
     "D488": "gaji guru honorer",
     "D10": "gaji guru honorer",
     "D474": "gaji guru honorer",
@@ -451,7 +451,7 @@ label_dict = {
     "D75": "gaji guru honorer",
     "D194": "gaji guru honorer",
     "D529": "gaji guru honorer",
-    "D231": "gaji guru honorer", # Mentok di D231 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D231": "gaji guru honorer", # Mentok di D231 term set gaji guru honorer (jumlah 22)
     "D238": "gaji guru honorer",
     "D289": "gaji guru honorer",
     "D18": "gaji guru honorer",
@@ -473,7 +473,7 @@ label_dict = {
     "D493": "gaji guru honorer",
     "D366": "gaji guru honorer",
     "D211": "gaji guru honorer",
-    "D1": "gaji guru honorer", # Mentok di D1 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D1": "gaji guru honorer", # Mentok di D1 term set gaji guru honorer (jumlah 22)
     "D502": "gaji guru honorer",
     "D31": "gaji guru honorer",
     "D452": "gaji guru honorer",
@@ -495,7 +495,7 @@ label_dict = {
     "D85": "gaji guru honorer",
     "D168": "gaji guru honorer",
     "D9": "gaji guru honorer",
-    "D88": "gaji guru honorer", # Mentok di D88 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D88": "gaji guru honorer", # Mentok di D88 term set gaji guru honorer (jumlah 22)
     "D329": "gaji guru honorer",
     "D396": "gaji guru honorer",
     "D27": "gaji guru honorer",
@@ -517,7 +517,7 @@ label_dict = {
     "D311": "gaji guru honorer",
     "D82": "gaji guru honorer",
     "D183": "gaji guru honorer",
-    "D242": "gaji guru honorer", # Mentok di D242 iterasi 1 term set gaji guru honorer (jumlah 22)
+    "D242": "gaji guru honorer", # Mentok di D242 term set gaji guru honorer (jumlah 22)
     "D347": "gaji guru honorer",
     "D167": "gaji guru honorer",
     "D137": "gaji guru honorer",
@@ -539,11 +539,11 @@ label_dict = {
     "D117": "gaji guru honorer",
     "D119": "gaji guru honorer",
     "D87": "gaji guru honorer",
-    "D172": "gaji guru honorer", # Mentok di D172 iterasi 1 term set gaji guru honorer (jumlah 22)
-    "D14": "gaji honorer", # Iteration 2
-    "D107": "honorer", # Iteration 3
+    "D172": "gaji guru honorer", # Mentok di D172 term set gaji guru honorer (jumlah 22)
+    "D14": "gaji honorer", # gaji honorer
+    "D107": "honorer", # honorer
     "D139": "honorer",
-    "D249": "gaji", # Iteration 4
+    "D249": "gaji", # gaji
     "D456": "gaji",
     "D216": "gaji",
     "D254": "gaji",
